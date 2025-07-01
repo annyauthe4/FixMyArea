89996ebdabfc8aac27df638e759b04cb3b080f21
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-07-02 00:27:19 +0100

    Include configuration file

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Configuration for the database setup."""
+
+
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "super-secret")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///fixmyarea.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
