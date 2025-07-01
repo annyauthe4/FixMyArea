3be6a37d500264d3cdb11f323824fc0500efb849
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-07-02 00:09:48 +0100

    Create db app

---
+++ backend/app/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Database app"""
+
+
+# app/__init__.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.routes import register_routes
+    register_routes(app)
+
+    return app
