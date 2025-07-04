ae60fa438742b9e793d0e941bf957531df0d7c22
Gemechis Chala <gladsonchala@gmail.com>
2025-07-04 16:18:18 +0300

    feat: reorganize router configuration for improved readability and structure

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,23 +5,38 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomeView
+    component: HomeView,
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: () => import('@/views/DashboardView.vue')
+    component: () => import('@/views/DashboardView.vue'),
+  },
+  {
+    path: '/agency',
+    name: 'Agency',
+    component: () => import('@/views/AgencyView.vue'),
   },
   {
     path: '/report',
     name: 'Report',
-    component: () => import('@/views/ReportView.vue')
-  }
+    component: () => import('@/views/ReportView.vue'),
+  },
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: () => import('@/views/AuthView.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('@/views/NotFoundView.vue'),
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
 })
 
 export default router
